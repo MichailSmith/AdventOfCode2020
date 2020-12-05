e473f4e7752d97c1a6452e3a119a27b5a073e94b
Michail Smith <misha@gomerits.com>
2020-12-04 22:23:29 -0800

    Add solution for day 2

---
+++ day2/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	data, err := ioutil.ReadFile("day2/input.txt")
+
+	if err != nil {
+		fmt.Println("Error opening file", err)
+	}
+
+	passwordStrings := strings.Split(string(data), "\n")
+	passwords := make([]passwordPolicy, len(passwordStrings))
+	regex := regexp.MustCompile(`(\d+)-(\d+) (.): (.*)`)
+
+	for i := range passwordStrings {
+		values := regex.FindStringSubmatch(passwordStrings[i])
+
+		min, err := strconv.Atoi(values[1])
+		if err != nil {
+			panic(err)
+		}
+
+		max, err := strconv.Atoi(values[2])
+		if err != nil {
+			panic(err)
+		}
+
+		passwords[i] = passwordPolicy{
+			minCount:  min,
+			maxCount:  max,
+			character: values[3],
+			password:  values[4],
+		}
+	}
+
+	validCount := 0
+
+	for i := range passwords {
+		password := passwords[i]
+
+		isValidRegex := regexp.MustCompile(password.character)
+
+		count := len(isValidRegex.FindAllString(password.password, -1))
+
+		if count >= password.minCount && count <= password.maxCount {
+			validCount++
+		}
+	}
+
+	fmt.Println(validCount)
+
+	validCount = 0
+
+	for i := range passwords {
+		password := passwords[i]
+
+		firstIndexContainsCharacter := password.password[password.minCount-1] == password.character[0]
+		secondIndexContainsCharacter := password.password[password.maxCount-1] == password.character[0]
+
+		if firstIndexContainsCharacter != secondIndexContainsCharacter {
+			validCount++
+		}
+	}
+
+	fmt.Println(validCount)
+}
+
+type passwordPolicy struct {
+	minCount  int
+	maxCount  int
+	character string
+	password  string
+}
